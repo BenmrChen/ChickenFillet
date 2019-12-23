4915dd0da5dfb4c28f170b6f2a283035e7e1bc32
Ben <mingren.chen16@gmail.com>
2019-12-23 17:41:01 +0800

    first commit

---
+++ app/Http/Controllers/ChickenFilletController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ChickenFillet;
+use Illuminate\Http\Request;
+
+class ChickenFilletController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ChickenFillet  $chickenFillet
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ChickenFillet $chickenFillet)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\ChickenFillet  $chickenFillet
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ChickenFillet $chickenFillet)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ChickenFillet  $chickenFillet
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ChickenFillet $chickenFillet)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ChickenFillet  $chickenFillet
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ChickenFillet $chickenFillet)
+    {
+        //
+    }
+}
